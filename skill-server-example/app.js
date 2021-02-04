75997d55ea16a1778176fc9943e1dc52e8c944f3
hjcho <hjcho@cware.co.kr>
2021-02-04 16:11:45 +0900

    1차 스킬 테스트 완료 - http request 요청 넘어오는거 확인

--- skill-server-example/app.js
+++ skill-server-example/app.js
@@ -1,7 +1,25 @@
+const APIkey = require('./myKey.js');
+
+var dnf = 'https://api.neople.co.kr/df/auction'
+
+var itemId;         //아이템ID
+var itemName;       //아이템명
+var count;          //아이템개수
+var unitPrice;      //개당 가격
+var currentPrice;   //현재가(총합)
+var avgPrice;       //평균가(개당)
+
+var jsonData; //json 파싱 데이터
+var url;      //json url
+var answer;   //최종 응답
+var botsay;   //봇이 할말
+
 const express = require('express');
 const app = express();
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const { request, response } = require('express');
+const { json } = require('body-parser');
 
 const apiRouter = express.Router();
 
@@ -13,7 +31,8 @@ app.use(bodyParser.urlencoded({
 
 app.use('/api', apiRouter);
 
-apiRouter.post('/sayHello', function(req, res) {
+//응답 예제
+apiRouter.post('/sayHello', function (req, res) {
   const responseBody = {
     version: "2.0",
     template: {
@@ -30,7 +49,8 @@ apiRouter.post('/sayHello', function(req, res) {
   res.status(200).send(responseBody);
 });
 
-apiRouter.post('/showHello', function(req, res) {
+//응답 예제 (카드형)
+apiRouter.post('/showHello', function (req, res) {
   console.log(req.body);
 
   const responseBody = {
@@ -50,6 +70,174 @@ apiRouter.post('/showHello', function(req, res) {
   res.status(200).send(responseBody);
 });
 
-app.listen(3000, function() {
+function setErrorTalk() {
+  botsay = '에러발생, 상담직원전환 -> 현재화면을 보내주시면 감사하겠습니다.';
+}
+
+// apiRouter.post('/frequentlyResources', function (req, res) {
+//   console.log(req.body);
+
+//   const question = req.body.user
+
+//   const responseBody = {
+//     version: "2.0",
+//     template: {
+//       outputs: [
+//         {
+//           simpleImage: {
+//             imageUrl: "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
+//             altText: "hello I'm Ryan"
+//           }
+//         }
+//       ]
+//     }
+//   };
+
+//   res.status(200).send(responseBody);
+// });
+
+apiRouter.post("/findItem", function (req, res) {
+
+  var user_key = decodeURIComponent(req.body.user_key); // user's key
+  var type = decodeURIComponent(req.body.type); // message type
+  var content = decodeURIComponent(req.body.content); // user's message
+
+  //var content = encodeURI(content);
+  var content = encodeURI(req.body.userRequest.utterance);
+  content.replace("\n","");
+
+
+  // var user_key = 'asdf'
+  // var type = 'text'; // message type
+  // var content = '생명의 숨결'; // user's message
+
+  console.log(user_key);
+  console.log(type);
+  console.log(content);
+
+  function setMsg(msg) {
+    answer = {
+        'message': {
+            'text': msg
+        }
+    }
+  }
+
+  function lastCall() {
+    setMsg(botsay);
+    res.set({
+        'content-type': 'application/json'
+    }).send(JSON.stringify(answer));
+  }
+
+  function cannotFind() {
+    botsay = '아이템을 정확히 입력해주세요, 약칭은 개선해 나가겠습니다 ㅠㅠ';
+    lastCall();
+  }
+
+  function findItem(){
+    const request = require('request');
+    url = "https://api.neople.co.kr/df/auction?itemName=" + content + "&apikey=" + APIkey;
+    console.log('url 1=' + url);
+    request.get({uri:url}, function(error, response, body){
+
+      if(!error) {
+        jsonData = JSON.parse(body);
+        jsonData = jsonData.rows[0];
+        itemId = jsonData.itemId;
+        console.log(itemId);
+  
+        itemName = jsonData.itemName;
+        count = jsonData.count;
+        unitPrice = jsonData.unitPrice;
+        currentPrice = jsonData.currentPrice;
+        avgPrice = jsonData.avgPrice;
+
+        const responseBody = {
+          version: "2.0",
+          template: {
+            outputs: [
+              {
+                basicCard: {
+                  title: itemName,
+                  description:  "개수 : " + count + "\n" + 
+                                "개당 가격 : " + unitPrice + "\n" +
+                                "종합 가격 : " + currentPrice + "\n" +
+                                "평균가 : " + avgPrice,
+                  thumbnail:{
+                    imageUrl: "https://img-api.neople.co.kr/df/items/" + itemId
+                  }
+                }
+              }
+            ]
+          }
+        };
+      
+        res.status(200).send(responseBody);
+  
+      } else {
+        setErrorTalk();
+        lastCall();
+      }
+    });
+  }
+  //함수 선언 끝
+
+  // const userRequest = req.body.userRequest;
+
+  findItem();
+  console.log(req.body);
+
+  // const question = req.body.user
+
+  // botsay = '검색을 원하는 아이템명을 입력해주세요';
+  // lastCall();
+  // console.log(lastCall());
+
+  // const responseBody = {
+  //   version: "2.0",
+  //   template: {
+  //     outputs: [
+  //       {
+  //         basicCard: {
+  //           title: itemName,
+  //           description: "개수 : " + count + "\n" + "개당 가격 : " + unitPrice + "\n",
+  //           thumbnail:{
+  //             imageUrl: "https://img-api.neople.co.kr/df/items/" + itemId
+  //           }
+  //         }
+  //       }
+  //     ]
+  //   }
+  // };
+
+  // res.status(200).send(responseBody);
+
+  // const responseBody = {
+  //   version: "2.0",
+  //   template: {
+  //     outputs: [
+  //       {
+  //         simpleImage: {
+  //           imageUrl: "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
+  //           altText: itemName
+  //         }
+  //       }
+  //     ]
+  //   }
+  // };
+
+  // res.status(200).send(responseBody);
+});
+
+
+
+
+apiRouter.get('/keyboard', (req, res) => {
+  const data = { 'type': 'text' }
+  res.json(data);
+});
+
+app.listen(3000, function () {
   console.log('Example skill server listening on port 3000!');
 });
